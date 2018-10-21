478e54edc11ebada08b8510c7ad0fb53a41c694d
James Bartow <pyrimmcdaniels@gmail.com>
2018-10-21 11:21:09 -0500

    Add countagains

--- james_dice/game_rules/chronicles_of_darkness.js
+++ james_dice/game_rules/chronicles_of_darkness.js
@@ -1,13 +1,18 @@
+
 function countSuccesses(diceResults){
+    return CountAgains(diceResults,8)
+}
+
+module.exports = {
+    GetSuccesses : countSuccesses,
+    CountAgains 
+}
+function CountAgains(diceResults, AgainNumber) {
     let successes=0;
     for (var i=0; i< diceResults.length; i++){
-        if (diceResults[i] >7){
+        if (diceResults[i] >=AgainNumber){
             successes=successes+1;
         }
     }
     return successes;
-}
-
-module.exports = {
-    GetSuccesses : countSuccesses
 }
