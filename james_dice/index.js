30b02cd4efe58177db27a6a7e811377552aa46b4
James Bartow <pyrimmcdaniels@gmail.com>
2018-10-21 10:18:15 -0500

    delete useless code

--- james_dice/index.js
+++ james_dice/index.js
@@ -1,11 +1,4 @@
 const   readline= require("readline");
-console.log("Hello James!")
-
-// Comment are any line that starts with a slash
-
-/*
-or are surrounded like this
-*/
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -19,27 +12,6 @@ rl.on("line",line=>{
     rl.prompt("enter another die roll")
 })
 
-let a_number = 5;
-let a_string = "Some string";
-let a_decimal_number = 3.14;
-let an_array = [5, "String", a_number];
-let an_object = {
-    property: "value",
-    other_property: 5,
-    some_property_that_is_an_array: an_array
-}
-let a=an_object.some_property_that_is_an_array
-console.log("a; " + a);
-// setTimeout(function(){
-//     setInterval (function(){console.log("this is an interval")}, 500)
-
-// },1500)
-console.log("a_number: " + a_number);
-console.log("a_string " + a_string);
-console.log("an_array " + an_array);
-console.log("an_object " + an_object);
-
-console.log("A random number between 0 and 1 : " + rolldice())
 function rolldice(){
     return Math.floor( Math.random() *10+1);
 }
