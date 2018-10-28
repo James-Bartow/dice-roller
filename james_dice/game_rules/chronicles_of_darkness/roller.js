aa05bc0ca18d2a2f11270267e4e62040f0858e38
James Bartow <pyrimmcdaniels@gmail.com>
2018-10-28 10:30:33 -0500

    Inject Rules into roller as a dependency

--- james_dice/game_rules/chronicles_of_darkness/roller.js
+++ james_dice/game_rules/chronicles_of_darkness/roller.js
@@ -1,20 +1,22 @@
-const Rules = require('./rules')
-let rules= new Rules()
 const Dice = require ('../../dice/roller')
 class CodRoller {
+    constructor(rules){
+        this.rules=rules
+    }
     RollDicePool (poolsize,againslevel){
         let initialdice= Dice.RollDiceOfPoolSize(poolsize);
         return this.rerollagains(initialdice, againslevel)
     }
 
     rerollagains(initialdieresult, againslevel) {
-        let againsPoolSize= rules.CountAgains(initialdieresult,againslevel)
+        let againsPoolSize= this.rules.CountAgains(initialdieresult,againslevel)
         while(againsPoolSize >0){
             let diceResultOfAgainsPool = Dice.RollDiceOfPoolSize(againsPoolSize)
             initialdieresult = initialdieresult.concat(diceResultOfAgainsPool)
-            againsPoolSize = rules.CountAgains(diceResultOfAgainsPool,againslevel)
+            againsPoolSize = this.rules.CountAgains(diceResultOfAgainsPool,againslevel)
         }
         return initialdieresult
     }
+
 }
 module.exports = CodRoller
